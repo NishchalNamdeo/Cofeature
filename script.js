0d2c10d75675923756f0df99326c73e09647de7d
NishchalNamdeo <nishchal04namdeo@gmail.com>
2024-04-21 22:44:08 +0530

    filter

---
+++ script.js
@@ -0,0 +1,7 @@
+var crsr = document.querySelector("#cursor")
+var body = document.querySelector("body")
+
+body.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+})
